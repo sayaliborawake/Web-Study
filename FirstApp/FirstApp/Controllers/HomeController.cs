99b45da502e61910ef1d838ebf5f37958d2fade8
Sayali Borawake <sayali_borawake@persistent.co.in>
2016-03-03 16:12:59 +0530

    tq

---
+++ FirstApp/FirstApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using FirstApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ViewResult MyIndex()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greetings = hour < 12 ? "Good morning" : "Good afternoon";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse respose)
+        {
+            if (ModelState.IsValid)
+                return View("Thanks", respose);
+            else
+                return View();
+        }
+
+	}
+}
